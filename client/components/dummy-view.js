582c5699443131e9d79160b70b6518c1a0a1a2f9
Kemelbek <kemelbekirsaliev@gmail.com>
2019-12-08 18:37:41 +0600

    Commit

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -1,15 +1,52 @@
-import React, { useState } from 'react';
-
+import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
-import Head from './head'
 
 const Dummy = () => {
-  const [counter] = useState(0)
+  // const [counter] = useState(0)
   return (
-    <div>
-      <Head title="Hello" />
-      <div> Hello World {counter} </div>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+      height: '100%',
+      width: '100%',
+      padding: '2%',
+      justifyContent: 'space-around',
+      alignItems: 'flex-end',
+    }}
+    >
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        minWidth: '200px',
+
+      }}
+      ><div>row 1</div>
+        <div>row 2</div>
+      </div>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        minWidth: '600px',
+        flexGrow: '1',
+        justifyContent: 'space-between'
+      }}
+      >
+        <div>Header</div>
+        <div>Body</div>
+      </div>
+      <div style={{
+        display: 'flex',
+        minWidth: '100px',
+        height: 'fit-content',
+        marginLeft: '2px'
+      }}
+      >
+        <div>Money</div>
+        <div>Funny</div>
+      </div>
     </div>
   )
 }
