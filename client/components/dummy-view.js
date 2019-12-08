baacc092cef26c87aa47aa277a248c813be5eb24
Kemelbek <kemelbekirsaliev@gmail.com>
2019-12-08 18:56:48 +0600

    BootyStrap

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -5,47 +5,11 @@ import { bindActionCreators } from 'redux'
 const Dummy = () => {
   // const [counter] = useState(0)
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'row',
-      height: '100%',
-      width: '100%',
-      padding: '2%',
-      justifyContent: 'space-around',
-      alignItems: 'flex-end',
-    }}
-    >
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%',
-        minWidth: '200px',
-
-      }}
-      ><div>row 1</div>
-        <div>row 2</div>
-      </div>
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%',
-        minWidth: '600px',
-        flexGrow: '1',
-        justifyContent: 'space-between'
-      }}
-      >
-        <div>Header</div>
-        <div>Body</div>
-      </div>
-      <div style={{
-        display: 'flex',
-        minWidth: '100px',
-        height: 'fit-content',
-        marginLeft: '2px'
-      }}
-      >
-        <div>Money</div>
-        <div>Funny</div>
+    <div>
+      <div className="row">
+        <div className="col-12 col-lg-4">1</div>
+        <div className="col-12 col-lg-4">2</div>
+        <div className="col-12 col-lg-4">3</div>
       </div>
     </div>
   )
