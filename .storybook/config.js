f9daff187e746ca89bf8c24830391f4374ab90a6
Kemelbek <kemelbekirsaliev@gmail.com>
2019-12-19 15:27:02 +0600

    Storybook dependencies added

---
+++ .storybook/config.js
@@ -0,0 +1,6 @@
+import { configure } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+addDecorator(withInfo); 
+
+configure(require.context('../src', true, /\.stories\.js$/), module);
---
+++ src/inputs.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export default { title: 'Inputs' };
+
+export const withText = () => <input type="text" />
